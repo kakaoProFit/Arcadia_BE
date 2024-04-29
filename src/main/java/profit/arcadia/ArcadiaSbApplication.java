4ab07487e90bdf117d9c0b5e75e244768ea424a7
mango0422 <tom990422@gmail.com>
2024-04-29 18:38:16 +0900

    first commit

---
+++ src/main/java/profit/arcadia/ArcadiaSbApplication.java
@@ -0,0 +1,13 @@
+package profit.arcadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcadiaSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcadiaSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/profit/arcadia/ArcadiaSbApplicationTests.java
@@ -0,0 +1,13 @@
+package profit.arcadia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcadiaSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
