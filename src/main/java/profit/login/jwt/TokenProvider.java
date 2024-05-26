385787bed6bae1e29893518d6a85fedfeb6ab479
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2024-05-27 03:54:31 +0900

    .

--- src/main/java/profit/login/jwt/TokenProvider.java
+++ src/main/java/profit/login/jwt/TokenProvider.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.Base64;
 import java.util.Collections;
 import java.util.Date;
 
@@ -31,17 +30,19 @@ public class TokenProvider {
     @Value("${security.jwt.refresh-expiration-time}")
     private long REFRESH_TOKEN_EXPIRE_TIME_IN_MILLISECONDS;
 
-    @Value("${security.jwt.secret-key-encoding-base64}")
+    @Value("${security.jwt.secret-key}")
     private String secretKey;
 
     private Key key;
 
     @PostConstruct
     public void init() {
+        // Base64 디코딩된 비밀 키를 사용하여 Key 객체를 생성합니다.
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
+    // JWT 유효성 검증
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder()
@@ -51,19 +52,20 @@ public class TokenProvider {
             log.info("JWT token is valid");
             return true;
         } catch (UnsupportedJwtException | MalformedJwtException exception) {
-            log.error("JWT is not valid");
+            log.error("JWT is not valid: {}", exception.getMessage());
         } catch (SignatureException exception) {
-            log.error("JWT signature validation fails");
+            log.error("JWT signature validation fails: {}", exception.getMessage());
         } catch (ExpiredJwtException exception) {
-            log.error("JWT is expired");
+            log.error("JWT is expired: {}", exception.getMessage());
         } catch (IllegalArgumentException exception) {
-            log.error("JWT is null or empty or only whitespace");
+            log.error("JWT is null or empty or only whitespace: {}", exception.getMessage());
         } catch (Exception exception) {
             log.error("JWT validation fails", exception);
         }
         return false;
     }
 
+    // 액세스 토큰 생성
     public String createToken(Authentication authentication) {
         Date date = new Date(System.currentTimeMillis());
         Date expiryDate = new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRE_TIME_IN_MILLISECONDS);
@@ -78,6 +80,7 @@ public class TokenProvider {
         return token;
     }
 
+    // 리프레시 토큰 생성
     public String createRefreshToken(Authentication authentication) {
         Date date = new Date(System.currentTimeMillis());
         Date expiryDate = new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRE_TIME_IN_MILLISECONDS);
@@ -92,6 +95,7 @@ public class TokenProvider {
         return refreshToken;
     }
 
+    // 인증 정보 추출
     public Authentication getAuthentication(String token) {
         Claims claims = Jwts.parserBuilder()
                 .setSigningKey(key)
