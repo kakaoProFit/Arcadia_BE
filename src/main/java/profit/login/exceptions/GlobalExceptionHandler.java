f9b480857ec9cc3361f6e69eb7d8b1714d311b54
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2024-06-03 02:51:24 +0900

    ExceptionHandler 수정

--- src/main/java/profit/login/exceptions/GlobalExceptionHandler.java
+++ src/main/java/profit/login/exceptions/GlobalExceptionHandler.java
@@ -2,14 +2,19 @@ package profit.login.exceptions;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,13 +27,12 @@ public class GlobalExceptionHandler {
         if (exception instanceof BadCredentialsException) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
             errorDetail.setProperty("description", "The username or password is incorrect");
-
-            return errorDetail;
         }
 
         if (exception instanceof AccountStatusException) {
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+                errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
             errorDetail.setProperty("description", "The account is locked");
+
         }
 
         if (exception instanceof AccessDeniedException) {
@@ -48,8 +52,47 @@ public class GlobalExceptionHandler {
 
         if (exception instanceof DataIntegrityViolationException) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(404), exception.getMessage());
-            errorDetail.setProperty("description", "This email is already in use");
-        }
+            errorDetail.setProperty("description", "DB error");
+        } // 데이터 중복, 필수값 없음..
+
+        if (exception instanceof MethodArgumentNotValidException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(405), "Invalid method argument");
+            errorDetail.setProperty("description", "The provided arguments are not valid");
+        } // 유효성 검사를 통과하지 못했을 때
+
+//        if (exception instanceof ResponseStatusException) {
+//            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(((ResponseStatusException) exception).getStatusCode().value()), exception.getReason());
+//            errorDetail.setProperty("description", "A response status exception occurred");
+//
+//            return errorDetail;
+//        }
+
+        if (exception instanceof HttpMessageNotReadableException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(406), exception.getMessage());
+            errorDetail.setProperty("description", "The request body is not readable or is malformed");
+        }   //json 형식 오류 (아마 사용자 입장에선 이 예외는 실행되지 않음)
+
+        if (exception instanceof ConstraintViolationException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(407), exception.getMessage());
+            errorDetail.setProperty("description", "Database constraint violation");
+        } // JPA에서 데이터베이스 제약 조건을 위반 (404랑 어떻게 다르게 처리되는지 잘은 모르겠음)
+
+        if (exception instanceof EntityNotFoundException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(408), exception.getMessage());
+            errorDetail.setProperty("description", "The requested entity was not found");
+        } // JPA에서 엔터티를 찾을 수 없을 때 발생
+
+//        if (exception instanceof IllegalArgumentException) {
+//            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), exception.getMessage());
+//            errorDetail.setProperty("description", "Illegal argument provided");
+//        }
+
+        if (exception instanceof NullPointerException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(409), exception.getMessage());
+            errorDetail.setProperty("description", "A null pointer exception occurred");
+
+            return errorDetail;
+        } // 널 포인터 참조가 발생
 
 
         if (errorDetail == null) {
