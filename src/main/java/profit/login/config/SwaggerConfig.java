61148ff486ed04ccd6ab016893432556c611415c
KIM-KWAN-IL <118953934+KIM-KWAN-IL@users.noreply.github.com>
2024-06-03 00:13:42 +0900

    Add Token

--- src/main/java/profit/login/config/SwaggerConfig.java
+++ src/main/java/profit/login/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package profit.login.config;
 
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,14 +14,21 @@ public class SwaggerConfig {
     private static final String API_NAME = "API 명세서";
     private static final String API_VERSION = "0.0.1";
     private static final String API_DESCRIPTION = "Project API 명세서";
-
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
 
     @Bean
-    public OpenAPI OpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .name(SECURITY_SCHEME_NAME)))
                 .info(new Info()
                         .title(API_NAME)
                         .version(API_VERSION)
-                        .description(API_DESCRIPTION));
+                        .description(API_DESCRIPTION))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
     }
 }
