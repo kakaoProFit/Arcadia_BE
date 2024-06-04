b732794ad2ce5644e2dd18941dab3def8af72eac
권민우 <422mukgui@gachon.ac.kr>
2024-06-04 00:55:49 +0000

    Update WebConfig.java

--- src/main/java/profit/login/config/WebConfig.java
+++ src/main/java/profit/login/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns("https://arcadiaprofit.shop", "https://arcadia.p-e.kr","http://localhost:3000") // “*“같은 와일드카드를 사용
 //                .allowedOrigins("https://arcadia.p-e.kr", "https://localhost:3000")
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowedHeaders("*") 
                 .allowCredentials(true)
                 .maxAge(3600);
 
