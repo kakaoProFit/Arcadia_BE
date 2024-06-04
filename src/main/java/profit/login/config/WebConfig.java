a63ae9a5c0a65c1abf52333fafc23743d4132b28
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2024-06-04 11:44:43 +0900

    소셜 로그인 user builder 수정

--- src/main/java/profit/login/config/WebConfig.java
+++ src/main/java/profit/login/config/WebConfig.java
@@ -11,9 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://arcadia.p-e.kr", "http://localhost:3000") // “*“같은 와일드카드를 사용
+//                .allowedOriginPatterns("https://arcadia.p-e.kr", "http://localhost:3000") // “*“같은 와일드카드를 사용
+                .allowedOriginPatterns("*")
 //                .allowedOrigins("https://arcadia.p-e.kr", "https://localhost:3000")
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowedHeaders("*")
+                .exposedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
 
